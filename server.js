25fbc4110afbaaadd1fcf0aca33e27b38213209d
JianyiLee <jianyi@hydrap.com>
2017-11-16 15:18:33 +0800

    update: change public folder to www for chat

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     users = [];
 //specify the html we will use
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/www'));
 //bind the server to the 80 port
 //server.listen(3000);//for local test
 server.listen(process.env.PORT || 5000);//publish to heroku
