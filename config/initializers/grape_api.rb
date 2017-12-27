d163e97469a4a750868d32df8cb01a1d68f5bc1c
JianyiLee <jianyi@hydrap.com>
2017-12-27 19:49:37 +0800

    update: add config/initializers/grape_api.rb

---
+++ config/initializers/grape_api.rb
@@ -0,0 +1,21 @@
+class Grape::API
+
+  NOT_GOD_DENIED = 'not GOD, permission denied.'
+  NOT_GOD_OA_DENIED = 'not GOD, not OA, permission denied.'
+  NOT_INVENTOR_DENIED = 'not INVENTOR, permission denied.'
+  NOT_GOD_INVENTOR_DENIED = 'not GOD, not INVENTOR, permission denied.'
+  NOT_ORG_USR_DENIED = 'not organization user, permission denied.'
+
+  MISSING_ORG = 'no organization found.'
+  MISSING_INV = 'no invention found.'
+  MISSING_ORG_USR = 'no organization user found.'
+  MISSING_INV_USR = 'no invention user found.'
+  MISSING_USR = 'no user found.'
+  MISSING_ROL = 'no role found.'
+  MISSING_USR_ORG = 'no user organization found.'
+  MISSING_USR_INV = 'no user invention found.'
+  MISSING_USR_ROL = 'no user role found.'
+
+  HAS_ROL = 'already has role'
+
+end
