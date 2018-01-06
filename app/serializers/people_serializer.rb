353153cf752b9ca74e7336cc563c62d096525a61
JianyiLee <jianyi@hydrap.com>
2018-01-06 08:26:43 +0800

    update: add is_expired to PeopleSerializer

--- app/serializers/people_serializer.rb
+++ app/serializers/people_serializer.rb
@@ -1,6 +1,10 @@
 class PeopleSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :email, :citizenship, :time_zone,
+  attributes :id, :firstname, :lastname, :email, :citizenship, :time_zone, :is_expired,
     :home_address, :work_address, :organizations
+
+  def is_expired
+    object.expired?
+  end
 
   def home_address
     AddressSerializer.new(object.home_address) if object.home_address
