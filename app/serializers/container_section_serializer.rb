7349a8faba2a7369ae1c7a002bee4ac373898cdf
JianyiLee <jianyi@hydrap.com>
2018-04-03 10:33:53 +0800

    update: container_section_serializer for section comments

--- app/serializers/container_section_serializer.rb
+++ app/serializers/container_section_serializer.rb
@@ -4,27 +4,45 @@ class ContainerSectionSerializer < ActiveModel::Serializer
     :created_time, :updated_time
 
   def draw
-    {content: object.draw || ""}
+    {
+      content: object.draw || "",
+      comments: CommentSerializer.build_array(object.draw_comments)
+    }
   end
 
   def significance
-    {content: object.significance || ""}
+    {
+      content: object.significance || "",
+      comments: CommentSerializer.build_array(object.significance_comments)
+    }
   end
 
   def landscape
-    {content: object.landscape || ""}
+    {
+      content: object.landscape || "",
+      comments: CommentSerializer.build_array(object.landscape_comments)
+    }
   end
 
   def problem_summary
-    {content: object.problem_summary || ""}
+    {
+      content: object.problem_summary || "",
+      comments: CommentSerializer.build_array(object.problem_summary_comments)
+    }
   end
 
   def gap
-    {content: object.gap || ""}
+    {
+      content: object.gap || "",
+      comments: CommentSerializer.build_array(object.gap_comments)
+    }
   end
 
   def problem_significance
-    {content: object.problem_significance || ""}
+    {
+      content: object.problem_significance || "",
+      comments: CommentSerializer.build_array(object.problem_significance_comments)
+    }
   end
 
   def created_time
