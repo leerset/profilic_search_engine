933e1ec77bdb85e8778bd25badf78c0ed8a6d18f
JianyiLee <jianyi@hydrap.com>
2018-05-08 08:07:59 +0800

    debug: check array while eager_loading2

--- app/serializers/eager_load_collection_serializer.rb
+++ app/serializers/eager_load_collection_serializer.rb
@@ -1,6 +1,6 @@
 class EagerLoadCollectionSerializer < ActiveModel::Serializer::CollectionSerializer
   def initialize(array, options = {})
-    if options[:serializer].respond_to?(:eager_load_array) && array.any?
+    if options[:serializer].respond_to?(:eager_load_array)
       array = options[:serializer].eager_load_array(array)
     end
 
--- config/initializers/serializer.rb
+++ config/initializers/serializer.rb
@@ -1,8 +1,11 @@
 class ActiveModel::Serializer
 
   def self.build_array(array, serializer_options = {})
-    EagerLoadCollectionSerializer.new(array, {serializer: self}.merge(serializer_options))
+    if array.any?
+      EagerLoadCollectionSerializer.new(array, {serializer: self}.merge(serializer_options))
+    else
+      CollectionSerializer.new(array, serializer: self)
+    end
   end
 
 end
-
