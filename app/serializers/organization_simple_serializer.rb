ade4396ec1366c413b37758e25b149d6640162df
Justin Chan <chanman@chanman.net>
2018-05-21 14:19:52 -0700

    add status for simple organization serializer

--- app/serializers/organization_simple_serializer.rb
+++ app/serializers/organization_simple_serializer.rb
@@ -1,6 +1,6 @@
 class OrganizationSimpleSerializer < ActiveModel::Serializer
   attributes :id, :name, :business_address, :created_time, :updated_time,
-    :highest_role
+    :highest_role, :user_organization_status
 
   def highest_role
     user = instance_options[:user]
@@ -8,6 +8,14 @@ class OrganizationSimpleSerializer < ActiveModel::Serializer
     role = user.highest_role(object)
     return nil if role.nil?
     RoleSerializer.new(role)
+  end
+
+  def user_organization_status
+    user = instance_options[:user]
+    return nil if user.nil?
+    s = user.user_organization_statuses.find {|uos| uos.organization_id == object.id}
+    return nil if s.nil?
+    s.status
   end
 
   def self.eager_load_array(array)
